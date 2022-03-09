2e6359315399966dda453810efc05c62c509d2b8
César Ayuso Cervera <cayucer@upv.edu.es>
2022-03-09 10:32:05 +0100

    service cosas

--- QuizifyClases/BusinessLogic/Services/IServicio.cs
+++ QuizifyClases/BusinessLogic/Services/IServicio.cs
@@ -7,6 +7,19 @@ namespace Quizify.Services
 {
     public interface IServicio
     {
-        void addPregunta();
+        void addPreguntaVF(PreguntaVF preguntaVF);
+        void addPreguntaTest(PreguntaTest preguntaTest);
+        void addAlumno(Alumno alumno);
+        void addProfesor(Profesor profesor);
+        void addInstitucion(Institucion institucion);
+        void addExamen(Examen examen);
+
+        PreguntaVF getPreguntaVFById(int Id, int version);
+        PreguntaTest getPreguntaTestById(int Id, int version);
+        Alumno getAlumnoById(string correo);
+        Profesor getProfesorById(string correo);
+        Institucion GetInstitucionById(string correo);
+        Examen GetExamenById(int Id);
+
     }
 }
