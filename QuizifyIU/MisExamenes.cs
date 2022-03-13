3023bdbe26d797fac32ab07d25f6587d3cf2568e
César Ayuso Cervera <cayucer@upv.edu.es>
2022-03-13 16:19:31 +0100

    Añadir Servicio a todas las UI

--- QuizifyIU/MisExamenes.cs
+++ QuizifyIU/MisExamenes.cs
@@ -5,14 +5,18 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Quizify.Entities;
+using Quizify.Services;
 
 namespace QuizifyIU
 {
     public partial class MisExamenes : Form
     {
-        public MisExamenes()
+        private Servicio servicio;
+        public MisExamenes(Servicio servicio)
         {
             InitializeComponent();
+            this.servicio = servicio;
         }
     }
 }
