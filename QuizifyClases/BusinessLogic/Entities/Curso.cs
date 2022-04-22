67e88497530587c2163b30ddf9b486fe8aec013d
Paul López <plopmoi@upv.edu.es>
2022-04-22 18:35:55 +0200

    Entity Curso

---
+++ QuizifyClases/BusinessLogic/Entities/Curso.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quizify.Entities
+{
+    public partial class Curso
+    {
+        public Curso(int id, string nombre, string autor, int alumnosApuntados, int maxAlumnos, DateTime fechaCreacion)
+        {
+            this.ID = id;
+            this.nombre = nombre;
+            this.autor = autor;
+            this.alumnosApuntados = alumnosApuntados;
+            this.maxAlumnos = maxAlumnos;
+            this.fechaCreacion = fechaCreacion;
+        }
+
+        public int GetId()
+        {
+            return ID;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public string GetAutor()
+        {
+            return autor;
+        }
+
+        public int GetNumAlumnos()
+        {
+            return alumnosApuntados;
+        }
+
+        public DateTime GetFechaCreacion()
+        {
+            return fechaCreacion;
+        }
+    }
+}
