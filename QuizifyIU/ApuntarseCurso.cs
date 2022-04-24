2591000a597f3131ee1b668d909bd92fe646f33f
Paul López <plopmoi@upv.edu.es>
2022-04-24 19:09:11 +0200

    Alumno apuntado a Curso hecho

--- QuizifyIU/ApuntarseCurso.cs
+++ QuizifyIU/ApuntarseCurso.cs
@@ -28,11 +28,41 @@ namespace QuizifyIU
 
         private void comprobarDatos()
         {
+            //Habría que comprobar que un alumno no está apuntado en el mismo curso 2 veces
             string alumno = usuario.correo;
             string codigo = codBox.Text;
+            string contra = contraBox.Text;
             Curso cursoApuntado = servicio.GetCurso(codigo);
             string profesor = cursoApuntado.autor;
-            servicio.AddAlumnoACurso(alumno, codigo, profesor);
+            if (servicio.ExisteCurso(codigo))
+            {
+                if (!contra.Equals(cursoApuntado.GetContraseña()))
+                {
+                    DialogResult avisoContraInc = MessageBox.Show(this, "La contraseña introducida es incorrecta.", "Contraseña incorrecta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (cursoLleno(codigo))
+                {
+                    DialogResult avisoCursoLLeno = MessageBox.Show(this, "El curso ya está lleni", "Curso lleno", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    servicio.AddAlumnoACurso(alumno, codigo, profesor);
+                    DialogResult apuntadoCorrecto = MessageBox.Show(this, "Has sido apuntado correctamente al curso con código " + codigo + ".", "Alumno apuntado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (apuntadoCorrecto == DialogResult.OK) this.Close();
+                }
+            }
+            else
+            {
+                DialogResult avisoNoExist = MessageBox.Show(this, "El curso con el código " + codigo + " no existe.", "Curso no existe", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private bool cursoLleno(string codigo)
+        {
+            bool lleno = false;
+            Curso cursoApuntado = servicio.GetCurso(codigo);
+            if (cursoApuntado.GetMaxAlumnos() == cursoApuntado.GetNumAlumnos()) lleno = true;
+            return lleno;
         }
     }
 }
