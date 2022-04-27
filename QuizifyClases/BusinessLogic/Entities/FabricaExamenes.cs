8d06b1d679a8e7d7beab74198336f98b7d290dd2
Cristian <crirodb2@inf.upv.es>
2022-04-27 09:26:09 +0200

    Gestión estados quiz

--- QuizifyClases/BusinessLogic/Entities/FabricaExamenes.cs
+++ QuizifyClases/BusinessLogic/Entities/FabricaExamenes.cs
@@ -7,10 +7,10 @@ namespace Quizify.Entities
     {
         public Examen CrearExamen(int id, string titulo, string descripcion, string curso, string autor, int tiempo,
         DateTime fecha_creac, DateTime fecha_ini, DateTime fecha_fin, int intentos, int volver_atras, int errores_restan, 
-        int mostrar_resultados, List<int> preguntas)
+        int mostrar_resultados, List<int> preguntas, string CT)
         {
             return new Examen(id, titulo, descripcion, curso, autor, tiempo, fecha_creac, fecha_ini, fecha_fin, intentos,
-            volver_atras, errores_restan, mostrar_resultados, preguntas);
+            volver_atras, errores_restan, mostrar_resultados, preguntas, CT);
         }
     }
 }
