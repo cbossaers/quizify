7952b82e424f0d46f3b40a94d00c48554986a991
Paul López <plopmoi@upv.edu.es>
2022-05-11 12:33:01 +0200

    Form Comprar Bonos Empezada

---
+++ QuizifyIU/ComprarBono.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuizifyIU
+{
+    public partial class ComprarBono : Form
+    {
+        public ComprarBono()
+        {
+            InitializeComponent();
+        }
+
+        private void bVolver_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bConfirmar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tarjetaBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void titularBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
