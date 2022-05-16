afd57e18f29c522af28118b5ea76a473b4d6c75d
Cristian <crirodb2@upv.edu.es>
2022-05-16 18:46:48 +0200

    Observador parte 1

--- QuizifyClases/BusinessLogic/Entities/Observador.cs
+++ QuizifyClases/BusinessLogic/Entities/Observador.cs
@@ -4,22 +4,29 @@ using System.Collections.Generic;
 using Quizify.Entities;
 using System.Threading;
 using System.IO;
+using Quizify.Persistence;
 
 namespace Quizify.Entities {
     public class Observador {
 
+        DALExamen dalex = new DALExamen();
+
+        public static string TextoNotif = "";
+
         public void test() {
 
         new Thread(() => {
-        Thread.CurrentThread.IsBackground = true; 
-        // Get the directories currently on the C drive.
-
-            // Write each directory name to a file.
+            int aux = dalex.UltimoIdExamen();
+            int aux2 = 0;
+            Thread.CurrentThread.IsBackground = true; 
 
             for(int i = 0; i < 99999; i++) {
-
                 System.Threading.Thread.Sleep(5000);
-
+                aux2 = dalex.UltimoIdExamen();
+                if(aux2 > aux) {
+                    TextoNotif = dalex.Get(aux2).titulo;
+                }
+                aux = aux2;
             }
         }).Start();
     }
