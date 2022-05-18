73c005431533cb5ab75a1d93086fd958411cbbb0
Cristian <crirodb2@upv.edu.es>
2022-05-18 18:45:31 +0200

    Pruebas observador 2

--- AppTest/Program.cs
+++ AppTest/Program.cs
@@ -21,22 +21,20 @@ namespace AppTest {
             DALExamen examen = new DALExamen();
             DALCurso curso = new DALCurso();
 
-            //Observador obs = new Observador();
-
             NuevoServicio service = new NuevoServicio();
 
             FabricaExamenes fabrica = new FabricaExamenes();
 
-            //service.GenerarExamen("patricio@gmail.com", "GPR", 3, 30, DateTime.Now.AddDays(2), DateTime.Now.AddDays(17), 2, 0, 1, 0);
-            //List<dynamic> filtros = new List<dynamic>() { "x", null, null, null };
-            //DataTable x = service.GetPreguntas(filtros);
+            var subject = new Sujeto();
+            var observerA = new Predicado();
+            subject.Attach(observerA);
 
-            //obs.test();
+            subject.SomeBusinessLogic();
 
-            //Console.WriteLine(x.Rows[1]["enunciado"]);
-            //Console.WriteLine(x.Rows[2]["id"]);
+            subject.Detach(observerA);
 
-            service.CalcularNotaExamen(5, "c");
+            subject.SomeBusinessLogic();
+
         }
         }
     }
