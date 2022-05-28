66480b7b55b43e1ec8dd890dc8c4ccec472d3bf1
César Ayuso Cervera <cayucer@upv.edu.es>
2022-05-28 17:25:50 +0200

    Crear Proyecto UT

---
+++ UniTestingDDS/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quizify.Entities;
+using Quizify.Persistence;
+using Quizify.Services;
+
+namespace UniTestingDDS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
