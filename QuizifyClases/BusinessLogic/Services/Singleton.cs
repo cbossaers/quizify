386d0ec03befe18ea840ba035f75da72a88f0b2a
César Ayuso Cervera <cayucer@upv.edu.es>
2022-05-29 18:23:36 +0200

    Singleton

---
+++ QuizifyClases/BusinessLogic/Services/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Quizify.Services;
+
+namespace Quizify.Entities
+{
+    public class Singleton
+    {
+        private static Alumno alumno;
+        private static Profesor profesor;
+        private NuevoServicio servicio = new NuevoServicio();
+
+        public Alumno GetAlumno(string correo)
+        {
+            if (alumno == null)
+            {
+                return servicio.GetAlumno(correo);
+            }
+            else
+            {
+                return alumno;
+            }
+        }
+        public Profesor GetProfesor(string correo)
+        {
+            if (profesor == null)
+            {
+                return servicio.GetProfesor(correo);
+            }
+            else
+            {
+                return profesor;
+            }
+        }
+
+    }
+}
